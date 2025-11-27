9209190ebae45807bfef13c8cfbea481cf97af7e
mayurp58 <mpawar.mayur@gmail.com>
2025-11-27 09:57:32 -0800

    force dynamic added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',  // prevent caching of static build files
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
